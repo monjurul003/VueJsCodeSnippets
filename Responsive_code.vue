1968859f53f65e7518aef034b453de7c4d19df6c
Russel M. Khan <mrkhan@1lifewss.com>
2020-01-22 12:06:53 -0600

    Added first file

---
+++ Responsive_code.vue
@@ -0,0 +1,31 @@
+<!-- Based on the size of the display this row will change the flex direction from row to column-->
+<template>
+	<v-card>
+		<v-row :style="direction">
+			<v-col></v-col>	
+			<v-col></v-col>	
+		</v-row>
+	</v-card>
+</template>
+<script>
+	
+export default{
+	components:{},
+	data(){
+		return {}
+	},
+	methods:{}, 
+	computed:{
+		direction(){
+            switch (this.$vuetify.breakpoint.name) {
+                case "xs":
+                case "sm":
+                case "md":
+                    return {"flex-direction" : "column"};
+                default:
+                    return {"flex-direction" : "row"};
+            }
+        }
+	} 
+}
+</script>
